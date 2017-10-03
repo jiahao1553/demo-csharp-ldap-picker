137bb79bc33dbb6043f39615d52d7753a765f3a8
JiaHao Tan <jiahaotan@outlook.com>
2017-10-03 16:49:07 +0800

    First commit

---
+++ csharp-ldap-user/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.ActiveDirectoryObjectPicker;
+
+namespace csharp_ldap_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryObjectPickerDialog picker = new DirectoryObjectPickerDialog()
+            {
+                AllowedObjectTypes = ObjectTypes.All,
+                DefaultObjectTypes = ObjectTypes.Users,
+                AllowedLocations = Locations.All,
+                DefaultLocations = Locations.JoinedDomain,
+                MultiSelect = true,
+                ShowAdvancedView = true
+            };
+
+
+            if (picker.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var sel in picker.SelectedObjects)
+                {
+                    Console.WriteLine(sel.Name);
+                }
+            }
+        }
+    }
+}
